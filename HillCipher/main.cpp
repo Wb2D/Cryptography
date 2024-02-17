a18b516753e39cc62dc481eb7be342e452c96fbc
Wb2D <ya.daniil2002.ru@ya.ru>
2024-02-18 02:52:34 +0300

    дописал шифр хилла

--- HillCipher/main.cpp
+++ HillCipher/main.cpp
@@ -3,6 +3,76 @@
 
 #include "matrix.h"
 
+/// I'm tired of leaving comments ...
+///
+/// USED:
+/// https://habr.com/ru/articles/332714/
+
+// Extended Euclidean algorithm also finds integer coefficients x and y such that: ax + by = gcd(a, b)
+int gcdExtended(int a, int b, int &x, int &y) {
+    if(a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int gcd = gcdExtended(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return gcd;
+}
+
+Matrix Encode(const Matrix &data, const Matrix &key) {
+    Matrix result(data.getRows(), data.getColumns());
+    QTextStream out(stdout, QIODevice::WriteOnly);
+    out << "D = " << QString::number(key.determinant()) << endl;
+    // split data and multiply each fragment by the key matrix : E = C * K
+    for(int i = 0; i < data.getRows(); ++i) {
+        result.replace("row", data.cut("row", i) * key, i);
+    }
+    // alphabet: English + _
+    return result % 27;
+}
+
+Matrix Decode(const Matrix &data, const Matrix &key) {
+    int det = key.determinant(), x, y;
+    QTextStream out(stdout, QIODevice::WriteOnly);
+    out << "D = " << QString::number(det) << endl;
+    int gcd = gcdExtended(27, det, x, y);
+    if(gcd != 1) {
+        out << "\nGCD(dit, 27) != 1 => Dinv not exist\n" << flush;
+    }
+    int invDet = 0;
+    // обратный детерминанту элемент в кольце по модулю [мощности алфавита], в моем случа 27
+    if(det < 0 && y > 0) {
+        invDet = y;
+    } else if(det > 0 && y < 0) {
+        invDet = 27 + y;
+    } else if(det > 0 && y > 0) {
+        invDet = y;
+    } else if(det < 0 && y < 0) {
+        invDet = -y;
+    }
+    Matrix inverseKey = (key.getAdjMatrix() % 27 * invDet % 27).transpose();
+
+    for(int i = 0; i < inverseKey.getRows(); ++i) {
+        for(int j = 0; j < inverseKey.getColumns(); ++j) {
+            if(inverseKey(i, j) < 0) {
+                inverseKey(i, j) += 27;
+            }
+        }
+    }
+    out << "Inv key matrix:\n" << (inverseKey).toString("-f") << flush;
+    out << "Key * Inv_key:\n" << ((key * inverseKey) % 27).toString("-f") << flush;
+    Matrix result(data.getRows(), data.getColumns());
+    // split data and multiply each fragment by the key matrix : D = E * K
+    for(int i = 0; i < data.getRows(); ++i) {
+        result.replace("row", data.cut("row", i) * inverseKey, i);
+    }
+    // alphabet: English + _
+    return result % 27;
+}
+
 int main(int argc, char *argv[])
 {
     // avoid compiler warnings
@@ -21,7 +91,7 @@ int main(int argc, char *argv[])
     // control menu
     while(QString::compare(userCommand, "exit")) {
        // input matrix size
-        out << "Input size matrix key (NxN):  = " << flush;
+        out << "Input size matrix key (NxN): " << flush;
         matrixValue = in.readLine().toInt();
         // input key
         out << "Input key: " << flush;
@@ -30,7 +100,6 @@ int main(int argc, char *argv[])
             out << "Key matrix must be square" << endl;
             break;
         }
-
         Matrix keyMatrix(key, matrixValue);
         // input data
         out << "Input data: " << flush;
@@ -41,15 +110,24 @@ int main(int argc, char *argv[])
         userCommand = in.readLine();
         // selecting a use case
         switch (listCommands.indexOf(userCommand)) {
-        case 0:
+        case 0: {
             // encoding
-
-            out << "Success!\nEncoded data: " << "\n>--------------------<" << endl;
+            out << "Key matrix:\n" << keyMatrix.toString("-f") << flush;
+            out << "Data matrix:\n" << dataMatrix.toString("-r") << flush;
+            Matrix resultMatrix = Encode(dataMatrix, keyMatrix);
+            out << "Encoded matrix:\n" << resultMatrix.toString("-r") << flush;
+            out << "Success!\nEncoded data: " << resultMatrix.toLetters(true) << "\n>--------------------<" << endl;
             break;
-        case 1:
+        }
+        case 1: {
             // decoding
-            out << "Success!\nDecoded data: " << "\n>--------------------<" << endl;
+            out << "Key matrix:\n" << keyMatrix.toString("-f") << flush;
+            out << "Data matrix:\n" << dataMatrix.toString("-r") << flush;
+            Matrix resultMatrix = Decode(dataMatrix, keyMatrix);
+            out << "Decoded matrix:\n" << resultMatrix.toString("-r") << flush;
+            out << "Success!\nDecoded data: " << resultMatrix.toLetters(true) << "\n>--------------------<" << endl;
             break;
+        }
         case 2:
             // exit
             out << "Exiting..." << endl;
@@ -62,13 +140,5 @@ int main(int argc, char *argv[])
     }
     // end of program
     out << "Program ended.\n" << endl;
-
-    /*
-    Matrix matrix(3, 3);
-    matrix.fillAuto(0, 26);
-
-    out << matrix.toString() << flush;
-    out << QString::number(matrix.determinant()) << flush;
-    */
     return 0;
 }
--- HillCipher/matrix.h
+++ HillCipher/matrix.h
@@ -12,16 +12,17 @@
 class Matrix
 {
 public:
+    // make empty vector size rows x columns
     Matrix(const int &rows, const int &columns) : _rows(rows), _columns(columns) {
         if(rows <= 0 || columns <= 0) {
-            throw std::invalid_argument("Matrix size is wrong : Matrix(int, int)") ;
+            throw std::invalid_argument("Matrix size is wrong : Matrix(int, int)");
         }
         _data.resize(rows * columns);
     }
 
     Matrix(const QString &data, const int &col) :  _columns(col) {
         if(col <= 0 || col > 3) {
-            throw std::invalid_argument("Matrix size is wrong : Matrix(QString, int)") ;
+            throw std::invalid_argument("Matrix size is wrong : Matrix(QString, int)");
         }
         bool flagReg = data.at(0).isUpper();
         bool flagSize = data.length() % col == 0 ? true : false;
@@ -29,8 +30,11 @@ public:
             if(flagReg == ch.isUpper()) {
                 _data.push_back(ch.toLatin1() - (flagReg ? 'A' : 'a'));
             }
+            else if(ch == '_') {
+                _data.push_back(26);
+            }
             else {
-                throw std::invalid_argument("Various input registers : Matrix(QString, int)") ;
+                throw std::invalid_argument("Various input registers : Matrix(QString, int)");
             }
         }
         _rows = flagSize ? data.length() / col : data.length() / col + 1;
@@ -43,16 +47,23 @@ public:
         }
     }
 
+    Matrix(const QVector<int> data, const int &rows, const int &columns) : _rows(rows), _columns(columns) {
+        if(data.size() != rows * columns) {
+            throw std::invalid_argument("Size of data not consistent with size matrix : Matrix(QVector, int, int)");
+        }
+        this->_data = data;
+    }
+
     int &operator()( int row,  int column) {
         if (row < 0 || row >= _rows || column < 0 || column >= _columns) {
-            throw std::out_of_range("Index is out of range : operator(int, int)");
+            throw std::out_of_range("Index is out of range : operator()(int, int)");
         }
         return _data[row * this->_columns + column];
     }
 
     const   int &operator()(const int &row, const int &column) const {
         if (row < 0 || row >= _rows || column < 0 || column >= _columns) {
-            throw std::out_of_range("Index is out of range : operator(int. int)");
+            throw std::out_of_range("Index is out of range : operator()(int, int) const");
         }
         return _data[row * this->_columns + column];
     }
@@ -104,7 +115,27 @@ public:
         return result;
     }
 
-    Matrix cut(const QString &objKey, const int &num) {
+    Matrix operator*(const int &num) const {
+        Matrix result(this->_rows, this->_columns);
+        int i = 0;
+        while(i != this->_data.size()) {
+            result._data[i] = this->_data[i] * num;
+            ++i;
+        }
+        return result;
+    }
+
+    Matrix operator%(const int& num) const {
+        Matrix result(this->_rows, this->_columns);
+        int i = 0;
+        while(i != this->_data.size()) {
+            result._data[i] = this->_data[i] % num;
+            ++i;
+        }
+        return result;
+    }
+
+    Matrix cut(const QString &objKey, const int &num) const {
         const QStringList listObjects = {"row", "column"};
         switch(listObjects.indexOf(objKey)) {
         case 0: {
@@ -129,16 +160,68 @@ public:
         }
     }
 
+    void replace(const QString &objKey, const Matrix &data, const int &num) {
+        const QStringList listObjects = {"row", "column"};
+        switch(listObjects.indexOf(objKey)) {
+        case 0: {
+            if(this->_columns != data._columns) {
+                throw std::invalid_argument("Columns in the matrices is unequal : replace(QString, Matrix, int)");
+            }
+            if(data._rows != 1) {
+                throw std::invalid_argument("Replacement row > 1 : replace(QString, Matrix, int)");
+            }
+            for(int i = 0; i < this->_columns; ++i) {
+                (*this)(num, i) = data(0, i);
+            }
+            break;
+        }
+        case 1: {
+            if(this->_rows != data._rows) {
+                throw std::invalid_argument("Rows in the matrices is unequal : replace(QString, Matrix, int)");
+            }
+            if(data._columns != 1) {
+                throw std::invalid_argument("Replacement column > 1 : replace(QString, Matrix, int)");
+            }
+            for(int i = 0; i < this->_rows; ++i) {
+                (*this)(i, num) = data(i, 0);
+            }
+            break;
+        }
+        default:
+            throw std::invalid_argument("Wrong object key : replace(QString, Matrix, int)");
+            break;
+        }
+    }
+
+    Matrix transpose() const {
+        if(this->_rows != this->_columns) {
+            std::invalid_argument("Matrix not square : transpose()");
+        }
+        int size = this->_rows;
+        Matrix result(size, size);
+        if(size == 1) {
+            result = *this;
+        } else {
+            for(int i = 0; i < size; ++i) {
+                for(int j = 0; j < size; ++j) {
+                    result(i, j) = (*this)(j, i);
+                }
+            }
+        }
+        return result;
+    }
+
     int determinant() const {
         if(this->_rows != this->_columns) {
-            std::invalid_argument("Matrix not square : determinant");
+            std::invalid_argument("Matrix not square : determinant()");
         }
+        int result = 0;
         if(this->_rows == 1) {
-            return _data[0];
+            result = _data[0];
         } else if(this->_rows == 2) {
-            return _data[0] * _data[3] - _data[1] * _data[2];
+            result = _data[0] * _data[3] - _data[1] * _data[2];
         } else if(this->_rows == 3)  {
-            return _data[0] * _data[4] * _data[8] +
+            result = _data[0] * _data[4] * _data[8] +
                     _data[1] *_data[5] * _data[6] +
                     _data[2] * _data[3] * _data[7] -
                     _data[2] * _data[4] * _data[6] -
@@ -147,37 +230,98 @@ public:
         } else {
             std::invalid_argument("Matrix size > 3 : determinant()");
         }
+        return result;
     }
 
-    Matrix inverse(const Matrix &matrix) const {
-        if(matrix.getRows() != matrix.getColumns()) {
-            std::invalid_argument("Matrix not square : inverse(Matrix)");
+    int minor(const int &row, const int &column) const {
+        if(this->_rows != this->_columns) {
+            std::invalid_argument("Matrix must be square : minor(int, int)");
         }
-        int det = this->determinant();
-        if(det == 0) {
-            std::invalid_argument("det Matrix = 0 : inverse");
+        int size = this->_rows;
+        if(size < 2 || size > 3) {
+            std::invalid_argument("Unsupported matrix size : minor(int, int)");
         }
-        Matrix result(matrix.getRows(), matrix.getColumns());
-        for(int i = 0; i < matrix.getRows(); ++i) {
-            for(int j = 0; j < matrix.getColumns(); ++j) {
-                result(i, j) = matrix(j, i) / det;
+        QVector<int> data = {};
+        for(int i = 0; i < size; ++i) {
+            for(int j = 0; j < size; ++j) {
+                if(i != row && j != column) {
+                    data.push_back((*this)(i, j));
+                }
+            }
+        }
+        return Matrix(data, size - 1, size - 1).determinant();
+    }
+
+    int adj(const int &row, const int &column) const {
+        int result = this->minor(row, column);
+        return (row + column) % 2 == 0 ? result : -result;
+    }
+
+    Matrix getAdjMatrix() const {
+        Matrix result(this->_rows, this->_columns);
+        for(int i = 0; i < this->_rows; ++i) {
+            for(int j = 0; j < this->_columns; ++j) {
+                result(i, j) = this->adj(i, j);
             }
         }
         return result;
     }
 
-    QString  toString() const {
-        QString result = QString::number(this->_rows) + " x " + QString::number(this->_columns) + "\n";
-        for(int i = 0; i < this->_rows; ++i) {
-            for(int j = 0; j < this->_columns; ++j) {
-                result += QString("%1 ").arg((*this)(i, j), 4);
+    QString toString(const QString &flag) const {
+        const QStringList listObjects = {"-f", "-r", "-c"};
+        QString result = {};
+        switch(listObjects.indexOf(flag)) {
+        case 0: {
+            result += this->makeTable();
+            break;
+        }
+        case 1: {
+            for(int i = 0; i < this->_rows; ++i) {
+                result += this->cut("row", i).makeTable();
             }
-            result += "\n";
+            break;
+        }
+        default: {
+            throw std::invalid_argument("Wrong object key : replace(QString, Matrix, int)");
+            break;
+        }
+        }
+        return result;
+    }
+
+
+    QString toLetters(const bool &flag) const {
+        QString result = {};
+        for(const int &num : _data) {
+            if(num != 26) {
+                result.append(QChar(num + (flag ? 'A' : 'a')));
+            } else {
+                result.append(QChar('_'));
+            }
         }
         return result;
     }
 
 private:
+    QString makeTable() const {
+        QString result = QString::number(this->_rows) + " x " + QString::number(this->_columns) + "\n";
+        // Create the horizontal line
+        QString horizontalLine = "+";
+        for(int j = 0; j < this->_columns; ++j) {
+            horizontalLine += "------+";
+        }
+        horizontalLine += "\n";
+        result += horizontalLine;
+        for(int i = 0; i < this->_rows; ++i) {
+            result += "|";
+            for(int j = 0; j < this->_columns; ++j) {
+                result += QString(" %1 |").arg((*this)(i, j), 4);
+            }
+            result += "\n" + horizontalLine;
+        }
+        return result;
+    }
+
     int _rows;
     int _columns;
     QVector<int> _data;
